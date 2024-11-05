21ebbf9507c8d3daf60998b97c2eaa5948e93ad6
alhussein <husain.f4l@gmail.com>
2024-11-05 14:09:30 +0300

    skinior

--- src/app/enviroments/enviroment.ts
+++ src/app/enviroments/enviroment.ts
@@ -1,6 +1,9 @@
-export const environment = {
+export const environmenthome = {
     production: false,
-    apiUrl: 'https://toppik.jo/api',
+    apiUrl: 'http://192.168.1.78:3000/api',
+}; export const environment = {
+    production: false,
+    apiUrl: 'http://192.168.0.198:3000/api',
 };
 export const environmentserver = {
     production: false,
